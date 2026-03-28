b39b1d8fa93dba37f35f4603a152d139fea9f854
Rhittum <rhittum1802@gmail.com>
2026-03-28 14:59:43 +0530

    minor changes

--- quine.py
+++ quine.py
@@ -1,4 +1,4 @@
-self = "self = \"?\"\n\ndef printSelf():\n    for i in range(0, self.__len__()):\n        if self[i]==chr(63):\n            for j in range(0, self.__len__()):\n                if self[j]=='\\n':\n                    print(\"\\n\", end='')\n                elif self[j]=='\\t':\n                    print(\"\\t\", end='')\n                elif self[j]=='\\\':\n                    print(\"\\\\\", end='')\n                else:\n                    print(self[j], end='')\n        else:\n            print(self[i], end='')\n\n\nprintSelf()\n"
+self = "self = \"?\"\n\ndef printSelf():\n    for i in range(0, self.__len__()):\n        if self[i]==chr(63):\n            for j in range(0, self.__len__()):\n                if self[j]=='\\n':\n                    print(\"\\n\", end='')\n                elif self[j]=='\\\':\n                    print(\"\\\\\", end='')\n                else:\n                    print(self[j], end='')\n        else:\n            print(self[i], end='')\n\n\nprintSelf()\n"
 
 def printSelf():
     for i in range(0, self.__len__()):
@@ -6,8 +6,6 @@ def printSelf():
             for j in range(0, self.__len__()):
                 if self[j]=='\n':
                     print("\\n", end='')
-                elif self[j]=='\t':
-                    print("\\t", end='')
                 elif self[j]=='\\':
                     print("\\\\", end='')
                 else:
